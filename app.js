8b6c403bdfa6ba90536f6c9d0e4a6baf0a35c223
Smgy94 <Shanemcgeown@gmail.com>
2023-12-13 15:35:26 +0000

    Improving accessibility
    Added aria-hidden="true" to each of the answers to prevent them from being read out loud automatically.

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const faqs = document.querySelectorAll(".question-wrapper");
+const answers = document.querySelectorAll(".faq-answer");
 
-faqs.forEach((faq) => {
-  faq.addEventListener("click", toggleAccordion);
+faqs.forEach((faq, index) => {
+  faq.addEventListener("click", () => toggleAccordion(index));
 });
 
-function toggleAccordion() {
-  const faqToggle = this.getAttribute("aria-expanded");
-  const iconPlus = this.querySelector(".icon-plus");
-  const iconMinus = this.querySelector(".icon-minus");
+function toggleAccordion(index) {
+  const faqToggle = faqs[index].getAttribute("aria-expanded");
+  const answerToggle = answers[index].getAttribute("aria-hidden");
+  const iconPlus = faqs[index].querySelector(".icon-plus");
+  const iconMinus = faqs[index].querySelector(".icon-minus");
 
-  for (const faq of faqs) {
-    faq.setAttribute("aria-expanded", false);
-    faq.querySelector(".icon-plus").classList.remove("hide");
-    faq.querySelector(".icon-minus").classList.add("hide");
+  for (let i = 0; i < faqs.length; i++) {
+    faqs[i].setAttribute("aria-expanded", false);
+    faqs[i].querySelector(".icon-plus").classList.remove("hide");
+    faqs[i].querySelector(".icon-minus").classList.add("hide");
+    answers[i].setAttribute("aria-hidden", true);
   }
 
   if (faqToggle === "false") {
-    this.setAttribute("aria-expanded", true);
+    faqs[index].setAttribute("aria-expanded", true);
     iconPlus.classList.add("hide");
     iconMinus.classList.remove("hide");
+    answers[index].setAttribute("aria-hidden", false);
   }
 }
